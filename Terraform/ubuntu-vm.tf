934302e82888ebd2cd6f99a1abc142aa5647b868
Mohamed Shafiq <68889041+leshafiq@users.noreply.github.com>
2026-08-27 17:14:40 +0800

    Modify VM configurations for docker-01 and add vk-docker-02
    Updated VM configurations for docker-01 and added vk-docker-02 with specific settings.

--- Terraform/ubuntu-vm.tf
+++ Terraform/ubuntu-vm.tf
@@ -1,21 +1,21 @@
-resource "proxmox_vm_qemu" "your-vm" {
+resource "proxmox_vm_qemu" "docker-01" {
 
-  name        = "k3s-master-01"
-  target_node = "pve"
-  vmid        = "210"
+  name        = "docker-01"
+  target_node = "pve-01"
+  vmid        = "201"
 
   clone = "ubuntu-cloud"
   cpu {
     cores   = 2
-    sockets = 1
-    type    = "kvm64"
+    sockets = 2
+    type    = "host"
   }
 
-  memory           = 2048
+  memory           = 16384
   os_type          = "cloud-init"
   full_clone       = true
   automatic_reboot = false
-  balloon          = 2048
+  balloon          = 16384
 
   network {
     id     = 0
@@ -40,7 +40,7 @@ resource "proxmox_vm_qemu" "your-vm" {
       scsi0 {
         disk {
           storage = "local-lvm"
-          size    = "20G"
+          size    = "64G"
         }
       }
     }
@@ -52,15 +52,86 @@ resource "proxmox_vm_qemu" "your-vm" {
   }
 
   vga {
-    type = "qxl"
+    type = "std"
   }
 
   # Cloud-init settings
-  ipconfig0  = "ip=10.10.10.21/24,gw=10.10.10.1"
+  ipconfig0  = "ip=1.1.1.1/24,gw=10.10.10.1"
   nameserver = "1.1.1.1"
   ciuser     = "user"
   cipassword = var.CLOUD_PASS
-  sshkeys    = var.PUBLIC_SSH_KEY
+  sshkeys    = join("\n", var.PUBLIC_SSH_KEY)
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "vk-docker-02" {
+
+  name        = "vk-docker-02"
+  target_node = "pve-01"
+  vmid        = "202"
+
+  clone = "ubuntu-cloud"
+  cpu {
+    cores   = 2
+    sockets = 2
+    type    = "host"
+  }
+
+  memory           = 8192
+  os_type          = "cloud-init"
+  full_clone       = true
+  automatic_reboot = false
+  balloon          = 8192
+
+  network {
+    id     = 0
+    bridge = "vmbr0"
+    model  = "virtio"
+    tag    = 10
+  }
+
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  boot     = "c"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage = "local-lvm"
+          size    = "64G"
+        }
+      }
+    }
+  }
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
+
+  vga {
+    type = "std"
+  }
+
+  # Cloud-init settings
+  ipconfig0  = "ip=11.1.1.1/24,gw=10.10.10.1"
+  nameserver = "1.1.1.1"
+  ciuser     = "user"
+  cipassword = var.CLOUD_PASS
+  sshkeys    = join("\n", var.PUBLIC_SSH_KEY)
 
   lifecycle {
     ignore_changes = [
